3461139a2a16921237c2b9210f0178aba9118289
AliElgohary <alielgohary35@gmail.com>
2023-09-10 15:12:14 +0300

    adding login and register

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './modules/product/product.component';
 import { HomeComponent } from './modules/home/home.component';
+import { AuthModule } from './modules/auth/auth.module';
 
-const routes: Routes = [{
-  path: '',
-  component: HomeComponent,
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'products',
+    component: ProductComponent,
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- public/src/app/modules/home/home.module.ts
+++ public/src/app/modules/home/home.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { HeaderComponent } from 'src/app/core/header/header.component';
-
-
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    HeaderComponent,
-  ],
-  exports: [
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent],
+  exports: [],
+  imports: [CommonModule],
 })
-export class HomeModule { }
+export class HomeModule {}
