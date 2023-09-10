bc33ef75aea73f6c8300b723c77189b845b888d6
AliElgohary <alielgohary35@gmail.com>
2023-09-10 13:53:19 +0300

    Home

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+
   ],
   imports: [
     BrowserModule,
