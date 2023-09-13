5f1a6f624f1e06ba2d3eb46ac95e68814114e270
AliElgohary <alielgohary35@gmail.com>
2023-09-13 16:07:51 +0300

    navigate from home

--- public/src/app/core/header/header.component.ts
+++ public/src/app/core/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { User } from 'src/app/modules/auth/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -14,7 +15,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   private userSub!: Subscription;
   private currentUserSub!: Subscription;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.userSub = this.authService.isAuthenticated$.subscribe((authenticated) => {
@@ -33,6 +34,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   onLogout(): void {
     this.authService.logout();
+    this.router.navigate(['/']);
   }
 
   isUserAdmin(): boolean {
--- public/src/app/modules/home/home.module.ts
+++ public/src/app/modules/home/home.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 
+
 @NgModule({
   declarations: [HomeComponent],
   exports: [],
   imports: [CommonModule],
 })
-export class HomeModule {}
+export class HomeModule {
+}
