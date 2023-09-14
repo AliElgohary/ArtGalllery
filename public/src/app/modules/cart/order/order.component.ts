82a9bad4c2ab20cada00534551dfdc7f83433e8f
AliElgohary <alielgohary35@gmail.com>
2023-09-14 17:05:06 +0300

    adding cart

--- public/src/app/modules/cart/order/order.component.ts
+++ public/src/app/modules/cart/order/order.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.scss']
+  styleUrls: ['./order.component.scss'],
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
+  cartItems: any[] = [];
+
+  getCartItems() {
+    const cart = localStorage.getItem('cartItems');
+    if (!cart) {
+      console.log('Cart not found');
+    } else {
+      this.cartItems = JSON.parse(cart);
+    }
+    console.log(this.cartItems);
+  }
+
+  ngOnInit(): void {
+    this.getCartItems();
+  }
+  saveCartToLocalStorage(){
+    localStorage.setItem('cartItems', JSON.stringify(this.cartItems));
+  }
+  removeFromCart(item: any): void {
+    const index = this.cartItems.indexOf(item);
+    if (index !== -1) {
+      this.cartItems.splice(index, 1);
+      this.saveCartToLocalStorage();
+    }
+  }
 
 }
